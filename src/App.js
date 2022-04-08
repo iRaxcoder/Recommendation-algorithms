d0863a7c0d3e53502f7b511b460ae6e23b05791b
Randall <randms07@hotmail.com>
2022-04-08 11:56:05 -0600

    add header and footer

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
-import './App.css';
-
+import Menu from "./components/Menu.js"
+import Footer from "./components/Footer.js";
+import './App.css'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Menu/>
+     <Footer/>
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+
+import "./../styles/Footer.css"
+
+const Footer = () => (
+    <div className="footer__container"> 
+        <div className="information">
+            <h1>Tarea 1</h1>
+            <h1>Curso: Sistemas Expertos</h1>
+            <h1>Randall Mora Sánchez</h1>
+            <h1>2022</h1>
+        </div>
+    </div>
+)
+
+export default Footer;
